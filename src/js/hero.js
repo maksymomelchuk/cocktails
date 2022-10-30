7ffe9045f19ee71e93dcedb5f55b8cab383797ba
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-10-30 20:36:44 +0200

    tablet success

--- src/js/hero.js
+++ src/js/hero.js
@@ -13,15 +13,64 @@ input.addEventListener('click', onInputClick);
 function onInputClick(event) {
   if (event.path[0].classList.contains('select__item')) {
     console.log(select.selectedId);
-    fetchByLetter(select.selectedId);
+    try {
+      fetchByLetter(select.selectedId);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // const screenHeight = document.documentElement.clientHeight;
+    // console.log(screenHeight);
+    // const cardHeight = document
+    //   .querySelector('.coctails__list')
+    //   .getBoundingClientRect().top;
+    // console.log(cardHeight);
+    // window.scrollBy({
+    //   top: cardHeight * 100,
+    //   behavior: 'smooth',
+    // });
   }
 }
+const fillter__list = document.querySelector('.fillter__list');
+const getTabletTemplate = () => {
+  const filterArr = [];
+  for (let i = 65; i <= 90; i += 1) {
+    filterArr.push(
+      `<li class="item" data-type="item" data-id="&#${i}"><button type="button" class="fillter_button">&#${i}</button></li>`
+    );
+  }
+  for (let i = 49; i <= 57; i += 1) {
+    filterArr.push(
+      `<li class="item" data-type="item" data-id="&#${i}"><button type="button" class="fillter_button">&#${i}</button></li>`
+    );
+  }
+  filterArr.push(
+    `<li class="item" data-type="item" data-id="&#48"><button type="button" class="fillter_button">&#48</button></li>`
+  );
 
-// document.addEventListener('click', closeSelectOnClick);
-// function closeSelectOnClick(event) {
-//   if (!event.target.classList.contains('select__input')) {
-//     console.log(select.isOpen);
-//     select.close();
-//     document.removeEventListener('click', closeSelectOnClick);
-//   }
-// }
+  fillter__list.insertAdjacentHTML('beforeend', filterArr.join(''));
+  // return selectArr;
+};
+
+getTabletTemplate();
+
+fillter__list.addEventListener('click', fillterClick);
+let currentEl;
+function fillterClick(event) {
+  const symbol = event.target.textContent;
+
+  if (event.path[0].classList.contains('fillter_button')) {
+    if (currentEl) {
+      currentEl.classList.remove('fillter_button--current');
+    }
+    try {
+      fetchByLetter(symbol);
+    } catch (error) {
+      console.log(error);
+    }
+
+    currentEl = event.path[0];
+
+    event.path[0].classList.add('fillter_button--current');
+  }
+}
