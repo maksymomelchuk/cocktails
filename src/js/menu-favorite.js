83eb815b6c734d980bebcec20e6064c95d3afab7
Oleg Gavelya <oleggavelya@gmail.com>
2022-11-01 10:17:51 +0200

    add burger menu favorite

--- src/js/menu-favorite.js
+++ src/js/menu-favorite.js
@@ -1,9 +1,11 @@
 const refs = {
   openFavorite: document.querySelector('.js-link-favorite'),
   menuFavorite: document.querySelector('.header-list-menu'),
+  openBurgerFavorite: document.querySelector('.js-burger-favorite'),
 };
 
 refs.openFavorite.addEventListener('click', openFavoriteMenu);
+refs.openBurgerFavorite.addEventListener('click', openFavoriteMenu);
 
 function openFavoriteMenu(e) {
   e.preventDefault();
