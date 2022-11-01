e5f6fbbc125172fc1c90f9fdd849a31f89f422b4
Oleg Gavelya <oleggavelya@gmail.com>
2022-11-01 21:40:32 +0200

    add toggle

--- src/js/menu-favorite.js
+++ src/js/menu-favorite.js
@@ -2,12 +2,18 @@ const refs = {
   openFavorite: document.querySelector('.js-link-favorite'),
   menuFavorite: document.querySelector('.header-list-menu'),
   openBurgerFavorite: document.querySelector('.js-burger-favorite'),
+  menuBurgerFavorite: document.querySelector('.header-list-burger-menu'),
 };
 
 refs.openFavorite.addEventListener('click', openFavoriteMenu);
-refs.openBurgerFavorite.addEventListener('click', openFavoriteMenu);
+refs.openBurgerFavorite.addEventListener('click', openFavoriteBurgerMenu);
 
 function openFavoriteMenu(e) {
   e.preventDefault();
   refs.menuFavorite.classList.toggle('visually-hidden');
+}
+
+function openFavoriteBurgerMenu(e) {
+  e.preventDefault();
+  refs.menuBurgerFavorite.classList.toggle('visually-hidden');
 }
