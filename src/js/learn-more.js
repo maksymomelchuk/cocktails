6384dc6a0041707825c3933662abd0f73292d137
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-01 14:43:00 +0200

    add modal learn more

---
+++ src/js/learn-more.js
@@ -0,0 +1,18 @@
+const openLearnMore = document.querySelector('.coctails__list');
+const closeLearnMore = document.querySelector('.coctails-close-btn');
+const learnMoreMenu = document.querySelector('.coctails-modal__backdrop');
+
+console.log(openLearnMore);
+openLearnMore.addEventListener('click', onOpenLearnMore);
+closeLearnMore.addEventListener('click', onCloseLearnMore);
+
+function onOpenLearnMore(event) {
+  console.log(event.target.classList);
+  if (event.target.classList.contains('learn-more-btn')) {
+    learnMoreMenu.classList.remove('is-close');
+  }
+}
+
+function onCloseLearnMore() {
+  learnMoreMenu.classList.add('is-close');
+}
