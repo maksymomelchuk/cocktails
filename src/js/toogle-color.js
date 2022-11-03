19386099fc2ab45c278213e48ebc25d486b136a2
maksymomelchuk <omelchukmaksym@gmail.com>
2022-11-04 00:03:28 +0200

    dark theme working

--- src/js/toogle-color.js
+++ src/js/toogle-color.js
@@ -5,4 +5,32 @@ const refs = {
 
 refs.label.addEventListener('click', () => {
   refs.body.classList.toggle('change-toggle-color');
+  const span = document.querySelectorAll('.heart-disactive');
+  console.log('span', span);
+  span.forEach(el => el.classList.toggle('dark'));
+  const p = document.querySelector('.hero__filter__text');
+  p.classList.toggle('dark');
+
+  const theme = localStorage.getItem('theme');
+  if (theme === null || theme === 'light') {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
+
+function checkTheme() {
+  const theme = localStorage.getItem('theme');
+  const span = document.querySelectorAll('.heart-disactive');
+  if (theme === null || theme === 'light') {
+    return;
+  } else {
+    const body = document.querySelector('body');
+    body.classList.toggle('change-toggle-color');
+    span.forEach(el => el.classList.toggle('dark'));
+    const p = document.querySelector('.hero__filter__text');
+    p.classList.toggle('dark');
+  }
+}
+
+setTimeout(checkTheme, 300);
