09f07ac0d41acd64311af12b8705ab8c3a7ead5b
Oleg Gavelya <oleggavelya@gmail.com>
2022-11-04 08:29:28 +0200

    change style-toggle

--- src/js/toogle-color.js
+++ src/js/toogle-color.js
@@ -1,8 +1,14 @@
 const refs = {
   body: document.querySelector('body'),
   label: document.querySelector('.theme-switch__label'),
+  input: document.querySelector('.burger-switch__input'),
 };
+console.log(refs.labelBurger);
 
 refs.label.addEventListener('click', () => {
+  refs.body.classList.toggle('change-toggle-color');
+});
+
+refs.input.addEventListener('click', () => {
   refs.body.classList.toggle('change-toggle-color');
 });
